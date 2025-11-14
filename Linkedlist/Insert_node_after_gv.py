9d281e0f3bf6f80a14059f182721be5c1e76e572
saivarshitnune <121888709+saivarshitnune@users.noreply.github.com>
2025-11-14 13:47:30 +0530

    Add insert_node_after_gv method to LinkedList
    Implemented a method to insert a node after a specified value in a linked list.

---
+++ Linkedlist/Insert_node_after_gv.py
@@ -0,0 +1,53 @@
+### Insert node after a given value will be applicable to all nodes from first to last.
+
+class Node:
+    def __init__(self,data=None,next=None):
+        self.data = data
+        self.next = None
+        
+class LinkedList:
+    def __init__(self,head=None):
+        self.head = head
+    
+    def add_node(self,var):
+        new_node = Node(var)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+            return 
+    
+    def print_ll(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+            
+    def insert_node_after_gv(self,data,var):
+        new_node = Node(data)
+        current = self.head
+        while current:
+            if current.data==var:
+                break
+            current = current.next
+            
+        if current is None:
+            print('No given value')
+            return
+        
+        
+        new_node.next = current.next
+        current.next = new_node
+        return      
+
+ll=LinkedList()
+ll.add_node(10)
+ll.add_node(20)
+ll.add_node(30)
+ll.add_node(40)
+ll.insert_node_after_gv(25,40)
+ll.print_ll()
