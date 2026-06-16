4cac8175d77eea20d61df90012f26c34699a2f26
saivarshitnune <121888709+saivarshitnune@users.noreply.github.com>
2026-06-16 12:25:16 +0530

    Rename function and update logic for max length

--- Arrays-Easy/longest_subarr_len_optimal.py
+++ Arrays-Easy/longest_subarr_len_optimal.py
@@ -2,19 +2,18 @@
 
 nums = [10, 5, 2, 7, 1, 9]
 k = 15  
-
-def count_sub_arr(arr,N,k):
+def len_sub_arr(arr,N,k):
     prefix_sum=0
     d = {0:-1}
-    count=0
+    max_length = 0
     for i in range(N):
         prefix_sum+=arr[i]
 
         remaining = prefix_sum - k
 
         if remaining in d:
-            count+=1
+            max_length = max(max_length,i-d[remaining])
 
         d[prefix_sum]=i
 
-    return count
+    return max_length
