3f8b26ee6ae9efdc951af87efe5a70224d45f738
saivarshitnune <121888709+saivarshitnune@users.noreply.github.com>
2026-06-16 10:03:56 +0530

    Update longest_subarr_len_optimal.py

--- Arrays-Easy/longest_subarr_len_optimal.py
+++ Arrays-Easy/longest_subarr_len_optimal.py
@@ -3,22 +3,18 @@
 nums = [10, 5, 2, 7, 1, 9]
 k = 15  
 
-def longest_sub_arry(arr,k):
-    n=len(arr)
+def count_sub_arr(arr,N,k):
     prefix_sum=0
-    d={}
-    result=0
-    for i in range(n):
+    d = {0:-1}
+    count=0
+    for i in range(N):
         prefix_sum+=arr[i]
-        
-        remaining=prefix_sum-k
-        
-        if remaining in d:
-            result = max(result,i-d[remaining])
-        
-        else:
-            d[prefix_sum]=i
-    
-    return result
 
-print(longest_sub_arry([-3, 2, 1],6))
+        remaining = prefix_sum - k
+
+        if remaining in d:
+            count+=1
+
+        d[prefix_sum]=i
+
+    return count
